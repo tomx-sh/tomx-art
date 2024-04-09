63c90d530999299adc075b2e04104a85d29c944f
Thomas DOUCHE <Thomas.douche@aol.com>
2024-04-09 02:16:33 +0200

    wider spacing and cursor reaction

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const margin = 20;
 const fontSize = 20;
-const vSpacing = fontSize * 1.5;
-const hSpacing = fontSize * 0.7;
+const vSpacing = fontSize * 2;
+const hSpacing = fontSize * 2;
 let font;
 const chars = "../|{@";
 
@@ -12,8 +12,8 @@ function preload() {
 
 
 function setup() {
-    createCanvas(400, 400);
-    frameRate(10);
+    createCanvas(700, 700);
+    frameRate(24);
 
     textFont(font);
     textSize(fontSize);
@@ -27,7 +27,8 @@ function draw() {
     background(20);
 
     const noiseScale = 0.01;
-    const noiseSpeed = 0.01;
+    const noiseSpeed = 0.001;
+    const cursorRadius = 250;
     
     for (let y = margin; y < height - margin; y += vSpacing) {
         for (let x = margin; x < width - margin; x += hSpacing) {
@@ -36,6 +37,14 @@ function draw() {
 
             var b = noise(x * noiseScale, y * noiseScale, frameCount * noiseSpeed); // 0 to 1
             b = adjustContrast(b);
+
+            let d = dist(mouseX, mouseY, x, y);
+            if (d < cursorRadius) {
+                b *= map(d, 0, cursorRadius, 0, 1); // Decrease b towards the cursor
+            }
+
+
+
             const charIndex = floor(map(b, 0, 1, 0, chars.length, true));
             const char = chars.charAt(charIndex);
 
